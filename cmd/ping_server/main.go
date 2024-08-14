9441e0cf387e6d5173d468d70eb88ce94d06eb0b
Andrew Ho <andyminhtuanho@gmail.com>
2024-08-14 01:37:52 -0700

    add prometheus metrics to test server

--- cmd/ping_server/main.go
+++ cmd/ping_server/main.go
@@ -3,11 +3,43 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
+
+var (
+	latencyHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_latency_seconds_total",
+			Help:    "Total latency of HTTP requests by method",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method"},
+	)
+	requestCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "requests_total",
+			Help: "Total number of gRPC requests",
+		},
+		[]string{"method"}, // Label for gRPC methods
+	)
+)
+
+func metricsMiddleware(handler http.HandlerFunc, handlerName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		handler(w, r)
+		latency := time.Since(start)
+
+		requestCounter.WithLabelValues(handlerName).Inc()
+		latencyHistogram.WithLabelValues(handlerName).Observe(latency.Seconds())
+	}
+}
 
 func main() {
 	var port int
@@ -20,12 +52,28 @@ func main() {
 	}
 	port = x
 
+	prometheus.MustRegister(latencyHistogram)
+	prometheus.MustRegister(requestCounter)
+
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		log.Println("metrics listening on", port+10)
+		log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", port+10), nil))
+	}()
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		_ = json.NewEncoder(w).Encode(map[string]any{
-			"message": fmt.Sprintf("Hello World from %d", port),
-		})
-	})
+	mux.HandleFunc("GET /",
+		metricsMiddleware(
+			func(w http.ResponseWriter, r *http.Request) {
+				_ = json.NewEncoder(w).Encode(
+					map[string]any{
+						"message": fmt.Sprintf("Hello World from %d", port),
+					},
+				)
+			},
+			"HelloWorld",
+		),
+	)
 
 	srv := http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
